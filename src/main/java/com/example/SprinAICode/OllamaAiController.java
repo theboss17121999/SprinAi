79d14db866c3b58c32cb541f8917858e5623ad75
theboss17121999 <46495612+theboss17121999@users.noreply.github.com>
2026-06-21 14:32:54 +0530

    first commit

---
+++ src/main/java/com/example/SprinAICode/OllamaAiController.java
@@ -0,0 +1,63 @@
+package com.example.SprinAICode;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.ai.ollama.OllamaChatModel;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+public class OllamaAiController {
+
+    private final ChatClient chatClient;
+    private final ChatMemory chatMemory = MessageWindowChatMemory.builder().build();
+
+    public OllamaAiController(OllamaChatModel chatModel) {
+
+        this.chatClient = ChatClient.builder(chatModel)
+                .defaultAdvisors(
+                        MessageChatMemoryAdvisor.builder(chatMemory).build()
+                )
+                .build();
+    }
+
+    @GetMapping("/apiOllama/{message}")
+    public ResponseEntity<String> getAnswer(@PathVariable String message) {
+
+        String response = chatClient.prompt()
+                .user(message)
+                .advisors(a -> a.param(ChatMemory.CONVERSATION_ID, "default"))
+                .call()
+                .content();
+
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/api/recommend")
+    public ResponseEntity<String> recommend(@RequestParam String type,
+                                            @RequestParam String year,
+                                            @RequestParam String lang) {
+        String tempt = """
+                    I want to watchh a {type} movie with {year} {lang}.
+                """;
+
+        PromptTemplate promptTemplate = new PromptTemplate(tempt);
+
+        Prompt prompt = promptTemplate.create(Map.of("type", type, "year", year, "lang", lang));
+
+        String response = chatClient
+                .prompt(prompt)
+                .advisors(a -> a.param(ChatMemory.CONVERSATION_ID, "recommend-session"))
+                .call()
+                .content();
+
+        return ResponseEntity.ok(response);
+    }
+}
---
+++ src/test/java/com/example/SprinAICode/SprinAiCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SprinAICode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprinAiCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
